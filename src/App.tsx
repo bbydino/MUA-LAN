efda938879d15c8280b40cdfea0bb29ec33250fe
bbydino <daniel.ngo16@gmail.com>
2021-06-24 23:09:17 -0500

    setup react app for typescript and eslint, installed some packages, added new logo/favicon

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>St. Andrew Dũng-Lạc Lion Dance Association</h1>
+    </div>
+  );
+}
+
+export default App;
